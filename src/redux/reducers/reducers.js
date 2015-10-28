61bada75e2ab9797bb7c78efb23375588fb77de8
Ricardo Pedroni <ric.pedroni@gmail.com>
2015-10-28 16:52:41 -0200

    Finish reducer for Hero movement control

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,6 +1,38 @@
-
-import { playerMoved } from '../actions/actions';
+import {
+  PLAYER_MOVED,
+  playerMoved,
+  PlayerMoveDirections
+} from '../actions/actions';
 
 const initialState = {
-  heroPosition: { x: 0, y: 0 }
+  boardSize: { width: 6, height: 5 },
+  heroPosition: { x: 0, y: 0 },
 };
+
+function app(state = initialState, action) {
+
+  switch (action.type) {
+    case PLAYER_MOVED:
+      return Object.assign({}, state, handleHeroMove(state.heroPosition, action.direction, state.boardSize));
+    default:
+      return state;
+  }
+}
+
+function handleHeroMove(position, direction, size) {
+  switch (direction) {
+    case PlayerMoveDirections.UP:
+      if (position.y > 0) out.y--;
+      break;
+    case PlayerMoveDirections.DOWN:
+      if (position.y < size.height) out.y++;
+      break;
+    case PlayerMoveDirections.LEFT:
+      if (position.x > 0) out.x--;
+      break;
+    case PlayerMoveDirections.RIGHT:
+      if (position.x < size.width) out.x++;
+      break;
+  }
+  return out;
+}
