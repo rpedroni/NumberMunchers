6b8a17a41706db1f07bc3e0beeca737bc96e9eb3
Ricardo Pedroni <ric.pedroni@gmail.com>
2015-10-30 21:35:49 -0200

    Add Redux DevTools into project

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+
+// Redux utility functions
+import { compose, createStore, applyMiddleware } from 'redux';
+// Redux DevTools store enhancers
+import { devTools, persistState } from 'redux-devtools';
+// React components for Redux DevTools
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 import { Provider } from 'react-redux';
+
 import App from './src/containers/App';
 
 // Reducers
 import appReducers from './src/redux/reducers/reducers';
 
-let store = createStore(appReducers, {
+const finalCreateStore = compose(
+  // Enables your middleware:
+  // applyMiddleware(m1, m2, m3), // any Redux middleware, e.g. redux-thunk
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+const store = finalCreateStore(appReducers, {
   boardSize: { width: 6, height: 4 },
   heroPosition: { x: 0, y: 0 },
-  numbers: []
+  values: []
 });
 
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+
+  </div>,
   document.getElementById('app')
 );
